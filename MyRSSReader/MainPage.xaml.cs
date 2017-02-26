b3ed321afd45ad7649352b3b315eebd9dcbbb973
TouchTheWorld <c1236548523@sina.cn>
2017-02-26 22:46:18 +0800

    添加项目文件。

---
+++ MyRSSReader/MainPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Web.Syndication;
+
+//“空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 上有介绍
+
+namespace MyRSSReader
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            //缓存大小,先这样吧
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //用了书上的回调
+            if (rssURL.Text != "")
+            {
+                RssRequest.GetRssItems(rssURL.Text,
+                   async (items) =>
+                   {
+                       await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                       {
+                           rsslistbox.ItemsSource = items;
+                       });
+                   },
+                   async (exception) =>
+                   {
+                       await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
+                       {
+                           await new MessageDialog(exception).ShowAsync();
+                       });
+
+                   });
+            }
+            else
+            {
+                await new MessageDialog("请输入RSS地址").ShowAsync();
+            }
+        }
+
+        private void rsslistbox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (rsslistbox.SelectedItem == null)
+                return;
+            var template = (RssModel)rsslistbox.SelectedItem;
+            Frame.Navigate(typeof(FullPage), template);
+            rsslistbox.SelectedItem = null;
+        }
+    }
+}
---
+++ MyRSSReader/RssModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MyRSSReader
+{
+    class RssModel
+    {
+        public string Img { get; set; }
+
+        public string Title { get; set; }
+
+        public string PublishedDate { get; set; }
+
+        public string Url { get; set; }
+
+        public string MainSummary { get; set; }
+
+        public RssModel(string title, string summary, string publishedDate, string url)
+        {
+            Title = title;
+            PublishedDate = publishedDate;
+            Url = url;
+            //获取主要文本
+            MainSummary = WebUtility.HtmlDecode(Regex.Replace(summary, "<[^>]+?>", ""));
+            //获取图片地址
+            Img = (summary.Remove(summary.IndexOf(".jpg") + 4)).Remove(0, summary.IndexOf('/', summary.IndexOf("img")) + 2);
+        }
+    }
+}
